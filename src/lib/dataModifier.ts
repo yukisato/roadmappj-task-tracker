762d7d3fc2eae1306fc84d037c71b78649b62df3
Yuki Sato <yuki.sato.dev@gmail.com>
2024-08-22 00:57:25 -0700

    refactor(file): make writeTasks() async

--- src/lib/dataModifier.ts
+++ src/lib/dataModifier.ts
@@ -1,12 +1,12 @@
 import { Task } from '@/types/task';
 import path from 'node:path';
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 
 export const dataFilePath = path.resolve(__dirname, '../.data.json');
 
-export const writeTasks = (tasks: Task[]): void => {
+export const writeTasks = async (tasks: Task[]): Promise<void> => {
   try {
-    fs.writeFileSync(dataFilePath, JSON.stringify(tasks), 'utf-8');
+    await fs.writeFile(dataFilePath, JSON.stringify(tasks), 'utf-8');
   } catch (error) {
     console.error(error);
     throw new Error('Failed to write tasks');
