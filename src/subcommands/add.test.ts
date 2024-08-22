1a38abcacd23a003c908cf4e55b119fa15776f62
Yuki Sato <yuki.sato.dev@gmail.com>
2024-08-22 13:07:10 -0700

    feat(test): separate a test into more declarative small tests

--- src/subcommands/add.test.ts
+++ src/subcommands/add.test.ts
@@ -22,23 +22,33 @@ describe('getNextId() returns an incremented next id', () => {
 });
 
 describe('add() adds a new task to the list', () => {
-  it('should create and append the data to the list', () => {
+  it('should create a task and add it to the list', () => {
+    const initialList: Task[] = [];
+    const description = 'task 1';
+    const updatedList = add(initialList, description);
+    assert.equal(updatedList.length, 1);
+
+    const actualTask = updatedList[0];
+
+    assert.equal(actualTask.id, 1);
+    assert.equal(actualTask.description, description);
+    assert.equal(actualTask.status, 'todo');
+  });
+
+  it('should be able to create tasks multiple times and increments the ids', () => {
     const initialList: Task[] = [];
     const description1 = 'task 1';
-    const newList1 = add(initialList, description1);
-    assert.equal(newList1.length, 1);
-    const newTask1 = newList1.find((task) => task.id === 1);
-    assert.notEqual(newTask1, undefined);
-    assert.equal(newTask1?.id, 1);
-    assert.equal(newTask1?.description, description1);
-    assert.equal(newTask1?.status, 'todo');
-
     const description2 = 'task 2';
-    const newList2 = add(newList1, description2);
-    assert.equal(newList2.length, 2);
-    const newTask2 = newList2.find((task) => task.id === 2);
-    assert.notEqual(newTask2, undefined);
-    assert.equal(newTask2?.description, description2);
-    assert.equal(newTask2?.status, 'todo');
+    const updatedList = add(add(initialList, description1), description2);
+    const actualTask1 = updatedList.find(
+      ({ description }) => description === description1
+    );
+    const actualTask2 = updatedList.find(
+      ({ description }) => description === description2
+    );
+    assert.equal(actualTask1?.id, 1);
+    assert.equal(actualTask1?.description, description1);
+    assert.equal(actualTask2?.id, 2);
+    assert.equal(actualTask2?.description, description2);
   });
 });
