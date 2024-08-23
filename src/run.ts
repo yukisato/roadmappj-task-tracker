e13b6b52657043f91ff4cecf2c4bdd33ab74948f
Yuki Sato <yuki.sato.dev@gmail.com>
2024-08-22 21:53:30 -0700

    feat(command): define Command type and its typeGuard function

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,26 @@
 import { dummy } from './subcommands/dummy';
+import { add, remove, update, markInProgress, markDone } from './subcommands';
+
+const commands = {
+  add: {
+    run: add,
+  },
+  update: {
+    run: update,
+  },
+  remove: {
+    run: remove,
+  },
+  'in-progress': {
+    run: markInProgress,
+  },
+  done: {
+    run: markDone,
+  },
+} as const;
+export type Command = keyof typeof commands;
+export const isCommand = (command: string): command is Command =>
+  Object.keys(commands).includes(command);
 
 export const run = (command?: string) => {
   if (!command) throw new Error('subcommand is not passed');
