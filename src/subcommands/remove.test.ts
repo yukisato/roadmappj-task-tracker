6dbb83d9413903af5c5b14d3b940aa35ac2f1dac
Yuki Sato <yuki.sato.dev@gmail.com>
2024-08-23 17:24:35 -0700

    chore: matches test titles in the same style

--- src/subcommands/remove.test.ts
+++ src/subcommands/remove.test.ts
@@ -5,16 +5,16 @@ import { Task } from '@/types/task';
 import { todoTask } from '../lib/testData';
 
 describe('remove() deletes a task from a list', () => {
-  it('returns [] if `list` is empty', () => {
+  it('should return [] if `list` is empty', () => {
     assert.deepEqual(remove([], 1), []);
   });
 
-  it('returns [] if `list` consisits of just one task with id 1 and `id` is 1', () => {
+  it('should return [] if `list` consisits of just one task with id 1 and `id` is 1', () => {
     const actual = remove([{ ...todoTask }], todoTask.id);
     assert.deepEqual(actual, []);
   });
 
-  it('returns the same list if the specified task with the id is not found', () => {
+  it('should return the same list if the specified task with the id is not found', () => {
     const original: Task[] = [{ ...todoTask }];
     const actual = remove(original, todoTask.id + 1);
     assert.deepEqual(actual, original);
