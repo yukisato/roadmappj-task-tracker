2330e0c93c3a3e411c723ed3668f5a3bda2579c7
Yuki Sato <yuki.sato.dev@gmail.com>
2024-08-27 00:02:48 -0700

    refactor(test): mock Date() and strictly check the expected time for created and updated time of tasks

--- src/subcommands/update.test.ts
+++ src/subcommands/update.test.ts
@@ -1,5 +1,5 @@
 import { Task } from '@/types/task';
-import { beforeEach, describe, it } from 'node:test';
+import { beforeEach, describe, it, mock } from 'node:test';
 import { update, updateArgsSchema } from './update';
 import assert from 'node:assert/strict';
 import { doneTask, inProgressTask, todoTask } from '../lib/testData';
@@ -19,10 +19,20 @@ describe('update() updates a task with the given id in the list', () => {
   });
 
   it('should update `updatedAt`', () => {
-    const actual = update(testList, todoTask.id, 'updated').find(
-      (task) => task.id === todoTask.id
-    );
-    assert.notEqual(actual?.updatedAt, todoTask.updatedAt);
+    mock.timers.enable({ apis: ['Date'] });
+    const list: Task[] = [
+      {
+        ...inProgressTask,
+        updatedAt: new Date().toISOString(),
+      },
+    ];
+
+    mock.timers.tick(1000);
+    const expected = new Date().toISOString();
+
+    assert.notEqual(list[0].updatedAt, expected);
+    const updated = update(list, inProgressTask.id, 'updated');
+    assert.equal(updated[0].updatedAt, expected);
   });
 
   it('should not update other tasks', () => {
